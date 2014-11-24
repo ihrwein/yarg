528555faeb3a4a98f67bf263efbf010783ad9d06
Tibor Benke <ihrwein@gmail.com>
2014-11-24 22:50:12 +0100

    application: add application.py skeleton

---
+++ yarg/application.py
@@ -0,0 +1,19 @@
+class Application:
+
+    def __init__(self):
+        pass
+
+    def add_new_profile(self, name):
+        pass
+
+    def remove_profile(self, name):
+        pass
+
+    def sync_profile(self, name, sync_observer):
+        pass
+
+    def save_configuration(self, ):
+        pass
+
+    def get_profiles(self):
+        pass
