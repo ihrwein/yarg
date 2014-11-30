f70959347465821f36d34f6dd41675c8a1b12e3e
Tibor Benke <ihrwein@gmail.com>
2014-11-30 11:36:11 +0100

    main: rename test_gui* to main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import qmlRegisterType, QQmlApplicationEngine
+
+from yarg.gui.main_controller import MainController
+from yarg.gui.profile_view_model import ProfileViewModel
+
+
+def main():
+    app = QGuiApplication(sys.argv)
+    qmlRegisterType(MainController, 'MainController', 1, 0, 'MainController')
+    qmlRegisterType(ProfileViewModel, 'ProfileViewModel', 1, 0, 'ProfileViewModel')
+    engine = QQmlApplicationEngine()
+    main_controller = MainController()
+    main_controller.profile_selection_changed(0)
+    engine.rootContext().setContextProperty('mainController', main_controller)
+    engine.load(QUrl.fromLocalFile('yarg/resource/main.qml'))
+    window = engine.rootObjects()[0]
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
